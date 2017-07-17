3075a08a24c1efd94b615775fa6e270ec01d9682
FreddieCodes <FrederickCodes@gmail.com>
2017-07-17 11:48:35 +0000

    Session 3:4

--- session3/challenge/4_hashes.rb
+++ session3/challenge/4_hashes.rb
@@ -5,3 +5,13 @@
 # There will be no punctuation in the strings.
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
+
+def first_pos(string)
+    to_return = {}
+    string.split.each_with_index { |k, v| to_return[k] ||= v} # ||= look into it's uses
+    to_return
+end
+
+
+# test
+p first_pos "The dog and the cat and the cow" 
