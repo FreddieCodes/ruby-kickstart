3edb8c0a0a78e717987c4edf20d0346e13406926
FreddieCodes <FrederickCodes@gmail.com>
2017-07-17 11:21:00 +0000

    Session 3 - challenge 3

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,12 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+def word_count(string)
+    words_count = Hash.new { |words, count| words[count] = 0}
+    string.downcase.split.each { |x| words_count[x] += 1 }
+    words_count
+end
+
+#test 
+p word_count("Hello the REST REST REST REST hi the") # => {"hello" => 1, "the" => 2, "rest" => 4, "hi" => 1}
