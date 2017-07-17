71c1bccbc69757120008573d98cd2bce6b87393b
FreddieCodes <FrederickCodes@gmail.com>
2017-07-17 10:29:36 +0000

    Session 3 - challenge 1

--- session3/challenge/1_blocks.rb
+++ session3/challenge/1_blocks.rb
@@ -1,5 +1,15 @@
 # Write a method, reverse_map, which invokes a block on each of the elements in reverse order,
-# and returns an array of their results.
+# and returns an array of their results.78
 #
 # reverse_map(1, 2, 3) { |i| i * 2 }      # => [6, 4, 2]
 
+def reverse_map(*array, &block)
+    array.reverse.map(&block)
+end
+
+# test could use a, b, c instead of *array
+# def reverse_map(a,b,c,, &block)
+#   [a,b,c].reverse.map(&block)
+# end
+
+p reverse_map(7,8,9) { |i| i } # => [9, 8, 7]
